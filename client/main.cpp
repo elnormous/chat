d0c81aedc55d3b2c02b5db1f731ff19aebb76b56
Elviss Strazdins <elviss@elviss.lv>
2018-05-21 04:58:40 +0300

    Handle connections

--- client/main.cpp
+++ client/main.cpp
@@ -4,15 +4,34 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <boost/asio/basic_socket.hpp>
+#include <boost/asio.hpp>
 #include "args.hxx"
+
+class Client final
+{
+public:
+    Client(boost::asio::io_service& s,
+           boost::asio::ip::tcp::resolver::iterator endpoints):
+        ioService(s),
+        socket(s)
+    {
+        boost::asio::async_connect(socket, endpoints, [this](boost::system::error_code e, boost::asio::ip::tcp::resolver::iterator) {
+            if (!e)
+                std::cout << "Connected" << std::endl;
+        });
+    }
+
+private:
+    boost::asio::io_service& ioService;
+    boost::asio::ip::tcp::socket socket;
+};
 
 int main(int argc, const char * argv[])
 {
     args::ArgumentParser parser("A simple command-line chat program.");
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
     args::ValueFlag<std::string> address(parser, "address", "Address of the server", {'a', "address"}, args::Options::Required);
-    args::ValueFlag<uint16_t> portNumber(parser, "portNumber", "Port number to connect to", {'p', "port"}, args::Options::Required);
+    args::ValueFlag<std::string> port(parser, "port", "Port number to connect to", {'p', "port"}, args::Options::Required);
     args::ValueFlag<std::string> nickname(parser, "nickname", "Nicname of the user", {'n', "nickname"}, args::Options::Required);
 
     try
@@ -34,6 +53,38 @@ int main(int argc, const char * argv[])
         std::cerr << e.what() << std::endl;
         std::cerr << parser;
         return EXIT_FAILURE;
+    }
+
+    try
+    {
+        boost::asio::io_service ioService;
+
+        boost::asio::ip::tcp::resolver resolver(ioService);
+
+        auto endpointIterator = resolver.resolve(address.Get(), port.Get());
+
+        Client client(ioService, endpointIterator);
+
+        // run io service in a separate thread
+        std::thread thread([&ioService]() {
+            ioService.run();
+        });
+
+        std::string line;
+        for (;;)
+        {
+            std::getline(std::cin, line);
+
+            std::cout << line << std::endl;
+
+            // TODO: encode and send message
+        }
+
+        thread.join();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
     return EXIT_SUCCESS;
--- server/main.cpp
+++ server/main.cpp
@@ -4,13 +4,56 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <boost/asio.hpp>
 #include "args.hxx"
+
+class Client final
+{
+public:
+    explicit Client(boost::asio::ip::tcp::socket sock):
+        socket(std::move(sock))
+    {
+        std::cout << "Client connected" << std::endl;
+    }
+
+private:
+    boost::asio::ip::tcp::socket socket;
+};
+
+class Server final
+{
+public:
+    Server(boost::asio::io_service& ioService,
+           const boost::asio::ip::tcp::endpoint& endpoint):
+        acceptor(ioService, endpoint),
+        socket(ioService)
+    {
+        std::cout << "Server started (port: " << endpoint.port() << ")" << std::endl;
+        accept();
+    }
+
+private:
+    void accept()
+    {
+        acceptor.async_accept(socket,
+                              [this](boost::system::error_code e) {
+                                   if (!e)
+                                       clients.push_back(Client(std::move(socket)));
+
+                                   accept();
+                               });
+    }
+
+    boost::asio::ip::tcp::acceptor acceptor;
+    boost::asio::ip::tcp::socket socket;
+    std::vector<Client> clients;
+};
 
 int main(int argc, const char * argv[])
 {
     args::ArgumentParser parser("A simple chat server.");
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-    args::ValueFlag<uint16_t> portNumber(parser, "port number", "Port number to listen to", {'p', "port"}, args::Options::Required);
+    args::ValueFlag<uint16_t> port(parser, "port", "Port number to listen to", {'p', "port"}, args::Options::Required);
 
     try
     {
@@ -31,6 +74,21 @@ int main(int argc, const char * argv[])
         std::cerr << e.what() << std::endl;
         std::cerr << parser;
         return EXIT_FAILURE;
+    }
+
+    try
+    {
+        boost::asio::io_service ioService;
+
+        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), port.Get());
+
+        Server server(ioService, endpoint);
+
+        ioService.run();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
     return EXIT_SUCCESS;
